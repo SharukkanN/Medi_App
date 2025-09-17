315741f08002c313c8702d6f2e52fe9a4029e2ed
Sangeeth Raveendran <rsangeeth246@gmail.com>
2025-09-17 13:44:05 +0530

    Refactor MyProfile; replace fetch calls with service methods for user data and appointments management

--- frontend/src/services/UserService.js
+++ frontend/src/services/UserService.js
@@ -1,7 +1,14 @@
-import { apiGet } from "../api/ApiManager";
+import { apiGet, apiPut } from "../api/ApiManager";
 
 export const getUserById = async (userId) => {
   return apiGet({
     path: `/users/${userId}`
   });       
+};
+
+export const updateUser = async (userId, userData) => {
+  return apiPut({
+    path: `/users/${userId}`,
+    requestBody: userData
+  });
 };
